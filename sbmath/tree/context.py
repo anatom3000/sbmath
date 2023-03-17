3be669ff25da439fab1e9bc1690a4e075c200a8c
Anatom <49984698+anatom3000@users.noreply.github.com>
2023-03-17 16:27:42 +0100

    add FunctionApplication node boilerplate

--- sbmath/tree/context.py
+++ sbmath/tree/context.py
@@ -1,14 +1,23 @@
+from __future__ import annotations
+
 from dataclasses import dataclass
 
-# from sbmath.tree import FunctionNode
+# from sbmath.tree import Function  # commented import for type annotations without circular imports
 
 
 @dataclass
 class Context:
-    functions: dict[str, FunctionNode]
+    functions: dict[str, Function]
 
-    def register_function(self, func: FunctionNode):
+    def register_function(self, func: Function):
         self.functions[func.name] = func
+
+    def get_function(self, name: str) -> Function:
+        return self.functions[name]
 
     def delete_function(self, name: str):
         del self.functions[name]
+
+
+class MissingContextError(Exception):
+    pass
--- sbmath/tree/functions.py
+++ sbmath/tree/functions.py
@@ -2,25 +2,78 @@ from __future__ import annotations
 
 import copy
 from abc import ABC, abstractmethod
+from typing import Optional
 
 from .base import Node, Variable, MatchResult, Value
+from .context import MissingContextError
 from .._utils import debug, inc_indent, dec_indent
 
 
-class FunctionNode(Node, ABC):
+class Function(ABC):
     name: str
 
-    def same_function(self, other: Node) -> bool:
-        return type(self) == type(other) and self.name == other.name
+    @abstractmethod
+    def reduce_func(self, _argument: Node, _depth: int) -> Optional[Node]:
+        return None
+
+    @abstractmethod
+    def can_evaluate(self, argument: Node) -> bool:
+        pass
+
+    @abstractmethod
+    def evaluate(self, argument: Node) -> Node:
+        pass
+
+
+class FunctionApplication(Node):
+
+    def approximate(self) -> float:
+        return self.function.evaluate(self.argument).approximate()
+
+    def evaluate(self) -> Node:
+        return self.function.evaluate(self.argument)
+
+    def reduce(self, depth=-1) -> Node:
+        if depth == 0:
+            return self
+
+        if self.is_evaluable():
+            return self.evaluate()
+
+        r = self.function.reduce_func(self.argument, depth)
+        if r is not None:
+            return r
+
+        return self.change_argument(self.argument.reduce(depth-1))
+
+    def is_evaluable(self) -> bool:
+        return self.function.can_evaluate(self.argument)
 
     def __eq__(self, other: Node) -> bool:
-        return self.same_function(other) and self.argument == other.argument
+        return isinstance(other, FunctionApplication) and self._function == other._function and self.argument == self.argument
+
+    @property
+    def function(self) -> Function:
+        if isinstance(self.function, str):
+            if context is None:
+                raise MissingContextError(f"could not get function '{self._function}' without context")
+            else:
+                return self.context.get_function(self._function)
+
+        return self._function
 
     def _replace_identifiers(self, match_result: MatchResult) -> Node:
-        return type(self)(self.argument._replace_identifiers(match_result))
+        return self.change_argument(self.argument._replace_identifiers(match_result))
+
+    def change_argument(self, new_argument: Node):
+        return type(self)(self._function, new_argument)
+
+    def __init__(self, function: str | Function, argument: Node):
+        self._function = function
+        self.argument = argument
 
     def replace(self, old: Node, new: Node):
-        return new if old.matches(self) else type(self)(self.argument.replace(old, new))
+        return new if old.matches(self) else self.change_argument(self.argument.replace(old, new))
 
     def contains(self, pattern: Node) -> bool:
         return pattern.matches(self) is not None or self.argument.contains(pattern)
@@ -29,7 +82,7 @@ class FunctionNode(Node, ABC):
         if not isinstance(value, type(self)):
             return None
 
-        if not self.same_function(value):
+        if self.function != value.function:
             return None
 
         return self.argument.matches(value.argument, state)
@@ -49,81 +102,11 @@ class FunctionNode(Node, ABC):
         if not isinstance(reduced_self, BaseFunctionNode):
             return reduced_self.matches(reduced_value, state)
 
+        # noinspection PyProtectedMember
         return reduced_self._match_no_reduce(reduced_value, state)
 
-    def is_evaluable(self) -> bool:
-        return self.argument.is_evaluable()
-
-    def __init__(self, argument: Node):
-        self.argument = argument
-
     def __str__(self):
-        return f"{self.name}({self.argument})"
+        return f"{self.function.name}({self.argument})"
 
     def __hash__(self):
         return hash(str(self))
-
-
-class PythonFunctionNode(FunctionNode):
-    source_fn: Callable[float, float]
-    special_values: dict[Node, Node] = {}
-
-    @property
-    def name(self):
-        return self.source_fn.__name__
-
-    @staticmethod
-    @abstractmethod
-    def _should_evaluate(argument: Node) -> bool:
-        pass
-
-    def reduce_no_eval(self, depth=-1) -> Node:
-        if depth == 0:
-            return self
-
-        reduced_arg = self.argument.reduce_no_eval(depth-1)
-
-        if reduced_arg in self.special_values.keys():
-            return self.special_values[reduced_arg]
-
-        return type(self)(reduced_arg)
-
-    def reduce(self, depth=-1) -> Node:
-        if depth == 0:
-            return self
-
-        reduced_arg = self.argument.reduce(depth - 1)
-        reduced_self = type(self)(reduced_arg)
-        inc_indent()
-        debug(f"{reduced_self = }", flag='match')
-        debug(f"{reduced_arg = }", flag='match')
-        dec_indent()
-        if self._should_evaluate(reduced_arg):
-            debug(f"should evaluate", flag='match')
-            return reduced_self.evaluate()
-
-        return reduced_self
-
-    def evaluate(self) -> Node:
-        if self.argument in self.special_values.keys():
-            return self.special_values[self.argument]
-
-        return Value(self.source_fn(self.argument.approximate()))
-
-    def approximate(self) -> float:
-        if self.argument in self.special_values.keys():
-            return self.special_values[self.argument].approximate()
-
-        return self.source_fn(self.argument.approximate())
-
-
-import math
-
-
-class Sin(PythonFunctionNode):
-    source_fn = math.sin
-    special_values = {Value(0.0): Value(0.0)}
-
-    @staticmethod
-    def _should_evaluate(argument: Node) -> bool:
-        return False
